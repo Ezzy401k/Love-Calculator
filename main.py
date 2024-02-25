cabed19f1ab29f84133d69b1e86ccc87e388ac93
Esrael Mekdem <esraelmekdem@gmail.com>
2024-02-25 16:28:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 print("Welcome to the Love Calculator!\n.")
+# input the names which will be converted to lower cases.
 name1 = input("What is your name? ").lower()
-name2 = input("Whar is their name? ").lower()
+name2 = input("What is their name? ").lower()
 
+# Concat the names to make a single string.
 name = name1 + " " + name2
+# Check the presence of the letters in the names given.
 t = name.count("t")
 r = name.count("r")
 u = name.count("u")
 e = name.count("e")
+# Add the count of the letters and change it to string.
 true = str(t + r + u + e)
-
+# Check the presence of the letters in the names given.
 l = name.count("l")
 o = name.count("o")
 v = name.count("v")
+# Add the count of the letters and change it to string.
 love = str(l + o + v + e)
 
+# concat the two strings and convert them to strings.
 true_love = int(true + love)
+# Do the comparison on the score.
 if true_love < 10 or true_love >90:
     print(f"Your score is {true_love}, you go together like coke and mentos.")
 elif true_love >= 40 and true_love <= 50:
